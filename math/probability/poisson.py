bed78c666bcf0981925015af308935c4e638c6c7
Morioh <m.onyonyi@alustudent.com>
2024-02-09 11:06:37 +0200

    Probability

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-# poisson.py
 
 """
-A module to represent and calculate properties of the Poisson distribution.
+Defines the Poisson class for Poisson distribution properties.
 
-This module provides the Poisson class, which encapsulates the functionality
-for representing a Poisson distribution. It includes methods for initializing
-the distribution with either raw data or a specific lambda (λ) value, which
-represents the expected number of occurrences within a given time frame.
+The Poisson distribution expresses the probability of a given number of events
+occurring in a fixed interval, assuming a constant mean rate and independence
+from the last event. This module allows initializing the distribution with data
+or a specified mean rate (lambda) and computing the probability mass function
+(PMF) for a given number of occurrences.
+
+Classes:
+    Poisson: Manages a Poisson distribution.
 """
 
 
@@ -19,15 +22,7 @@ class Poisson:
 
     def __init__(self, data=None, lambtha=1.):
         """
-        Initializes the Poisson instance with data or a specific lambtha value.
-
-        Args:
-            data (list, optional): Data to estimate the distribution.
-            lambtha (float, optional): Expected occurrences. Defaults to 1.
-
-        Raises:
-            ValueError: If lambtha <= 0 or data has < 2 values.
-            TypeError: If data is not a list.
+        Initializes the Poisson instance with data.
         """
         if data is None:
             if lambtha <= 0:
@@ -39,3 +34,28 @@ class Poisson:
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of successes.
+
+        Args:
+            k (int): The number of successes.
+
+        Returns:
+            float: The PMF value for k.
+        """
+        if not isinstance(k, int):
+            k = int(k)
+
+        if k < 0:
+            return 0
+
+        e = 2.7182818285
+        lambtha_k_factorial = 1
+        for i in range(1, k + 1):
+            lambtha_k_factorial *= i
+
+        pmf_value = (self.lambtha ** k) * \
+            (e ** (-self.lambtha)) / lambtha_k_factorial
+        return pmf_value
