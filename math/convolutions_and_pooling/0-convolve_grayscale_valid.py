12ecaed421cdeecd10f5f828c0d23e53745900e3
Morioh <m.onyonyi@alustudent.com>
2024-02-16 00:51:02 +0200

    0. Valid Convolution

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -11,7 +11,7 @@ import numpy as np
 def convolve_grayscale_valid(images, kernel):
     """
 Parameters:
-- images: numpy.ndarray of shape (i, y, x), where it contains multiple grayscale
+- images: numpy.ndarray of shape (i, y, x), where it contains multiple grayscl
   images. Here, 'm' denotes the number of images, 'y' is the height in pixels,
   and 'x' is the width in pixels of the images.
 - kernel: numpy.ndarray with shape (m, n), containing the convolution kernel.
